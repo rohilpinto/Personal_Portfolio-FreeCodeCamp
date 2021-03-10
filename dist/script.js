ed334bdbdc6c6e9cabd8a926c8fc60e174dd4030
rohilpinto <rohilcrispinto@gmail.com>
2021-03-10 16:36:33 +0530

    added ham button logic

--- dist/script.js
+++ dist/script.js
@@ -2,10 +2,26 @@ const hamMenuBtn = document.querySelector(".hamburger-menu");
 console.log(hamMenuBtn);
 
 const linksContainer = document.querySelector(".links-container");
+const body = document.body;
 
-hamMenuBtn.addEventListener("click", openHamMenu);
 
-function openHamMenu() {
-	linksContainer.classList.toggle('ham-links-open')
-	// adds class 'ham-links-open' and changes the left property to 0;
+// events
+
+ 
+
+
+window.addEventListener('click' , openHamMenu)
+
+function openHamMenu(e) {
+	console.log(e.target);
+
+
+	if(e.target.matches('.hamburger-menu')) {
+		linksContainer.classList.add('hamLinksOpen')
+	} else {
+		linksContainer.classList.remove('hamLinksOpen')
+	}
+
+
+
 }
